dd8715de26ed3b896855f5aacfdfb86d234a9612
LesPrimus <amerigo.armentano@hotmail.it>
2025-08-07 10:24:21 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -12,6 +12,20 @@ def main():
         if command.startswith("echo"):
             print(command[5:])
             continue
+        if command.startswith("type"):
+            value =  command[5:]
+            if value == "echo":
+                print("echo is a shell builtin")
+                continue
+            elif value == "exit":
+                print("exit is a shell builtin")
+                continue
+            elif value == "type":
+                print("type is a shell builtin")
+                continue
+            else:
+                print(f"{value}: not found")
+                continue
         print(f"{command}: command not found")
 
 if __name__ == "__main__":
